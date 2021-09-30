0995740dcd6fe5fa016c14a9aaaeb000d86a90a6
DoLT <dolt@runsytem.net>
2021-09-30 23:00:23 +0700

    update text

--- src/components/sections/introduction/IntroductionFour.js
+++ src/components/sections/introduction/IntroductionFour.js
@@ -35,22 +35,15 @@ function IntroductionFour() {
           `}`}
         />
         <div className="introduction-four-content">
-          <IcHeading title="Cam kết thỉ công đúng 100% những hạng mục công việc đã nêu trong hợp đồng." />
-          <IcHeading title="Tuyệt đối không đưa vật tư giả, vật tư kém chất lượng vào thi công công trình." />
-          <IcHeading title="Cam kết chịu phạt tiến độ theo đúng thỏa thuận trong hợp đồng." />
-          <IcHeading title="Giữ giá xây dựng tại thời điểm ký kết hợp đồng cho đến khi hoàn thành công trình." />
-          <IcHeading title="Không bán thầu dưới mọi hình thức" />
-          <IcHeading title="Cam kết chịu trách nhiệm pháp lý đối với công trình do công ty thỉ công theo đúng quy định của pháp luật Việt Nam." />
-          <IcHeading title="Sẵn lòng đền bù mọi thiệt hại vật chất cho công trình và nhà xung quanh nếu lỗi do công ty gây ra." />
-          <IcHeading title="Chế độ bảo hành chu đáo cùng đội ngũ nhân sự chuyên nghiệp và có trách nhiệm." />
-          <IcHeading title="Tuân thủ nghiêm ngặt các quy định an toàn lao động và an toàn phòng chống dịch dưới sự giám sát thường xuyên của Kỹ sư xây dựng và Phòng ATLĐ." />
-          <IcHeading title="Hỗ trợ kiểm tra, thẩm định kết cấu cũ." />
-          <IcHeading title="Hỗ trợ thủ tục và hồ sơ xỉn cấp phép nhà cải tạo." />
-          <IcHeading title="Hỗ trợ bản vẽ cải tạo nhà." />
-          <IcHeading
-            title="Hỗ trợ làm việc với ban dự án về hồ sơ pháp lý, thủ tục thi công công trình, thủ tục hoàn công với nhà dự án."
-            line={false}
-          />
+          {data.map((item, i) => {
+            return (
+              <IcHeading
+                key={i}
+                title={item.title}
+                line={i !== data.length - 1}
+              />
+            );
+          })}
         </div>
       </Container>
     </div>
