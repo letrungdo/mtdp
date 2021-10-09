3e88bdf45b6afeac909744b93c62b879e880dba9
DoLT <dolt@runsytem.net>
2021-10-09 21:09:58 +0700

    feat: add googletagmanager

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,25 @@
 import { DefaultSeo } from "next-seo";
+import { useRouter } from "next/dist/client/router";
+import { useEffect } from "react";
 import { Provider } from "react-redux";
-import { initializeStore } from "../redux/store";
+import { pageview } from "../lib/gtag";
 import SEO from "../next-seo.config";
+import { initializeStore } from "../redux/store";
 import "../styles/antd.less";
 import "../styles/styles.scss";
 
 const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Provider store={initializeStore()}>
       <DefaultSeo {...SEO} />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from "../lib/gtag";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -27,6 +28,21 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link href="/assets/css/slick.min.css" rel="stylesheet" />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+            }}
+          />
         </Head>
         <body>
           <Main />
